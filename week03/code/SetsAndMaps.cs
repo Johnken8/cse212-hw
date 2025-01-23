77d8406e538ebf373075bfe17659d92da15f329c
Johnken8 <ucheebuka162@gmail.com>
2025-01-23 07:04:57 +0100

    push

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -1,106 +1,191 @@
-using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
-public static class SetsAndMaps
+namespace Week03
 {
-    /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for returning all symmetric pairs of words.  
-    ///
-    /// For example, if words was: [am, at, ma, if, fi], we would return :
-    ///
-    /// ["am & ma", "if & fi"]
-    ///
-    /// The order of the array does not matter, nor does the order of the specific words in each string in the array.
-    /// at would not be returned because ta is not in the list of words.
-    ///
-    /// As a special case, if the letters are the same (example: 'aa') then
-    /// it would not match anything else (remember the assumption above
-    /// that there were no duplicates) and therefore should not be returned.
-    /// </summary>
-    /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    public static string[] FindPairs(string[] words)
+    public class SetsAndMaps
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
-    }
-
-    /// <summary>
-    /// Read a census file and summarize the degrees (education)
-    /// earned by those contained in the file.  The summary
-    /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
-    /// have earned that degree.  The degree information is in
-    /// the 4th column of the file.  There is no header row in the
-    /// file.
-    /// </summary>
-    /// <param name="filename">The name of the file to read</param>
-    /// <returns>fixed array of divisors</returns>
-    public static Dictionary<string, int> SummarizeDegrees(string filename)
-    {
-        var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename))
+        // Problem 1: FindPairs - O(n) solution using HashSet
+        public static string[] FindPairs(string[] words)
         {
-            var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var result = new List<string>();
+            var seen = new HashSet<string>();
+
+            foreach (var word in words)
+            {
+                // Skip words with same letters (e.g., "aa")
+                if (word[0] == word[1]) continue;
+                
+                string reversed = new string(word.Reverse().ToArray());
+                if (seen.Contains(reversed))
+                {
+                    result.Add($"{reversed} & {word}");
+                }
+                else
+                {
+                    seen.Add(word);
+                }
+            }
+
+            return result.ToArray();
         }
 
-        return degrees;
+        // Problem 2: SummarizeDegrees - Dictionary to count degrees
+        public static Dictionary<string, int> SummarizeDegrees(string filePath)
+        {
+            var degreeSummary = new Dictionary<string, int>();
+            
+            try
+            {
+                // Skip header row and process each line
+                foreach (var line in File.ReadLines(filePath).Skip(1))
+                {
+                    var columns = line.Split(',');
+                    if (columns.Length >= 4)
+                    {
+                        string degree = columns[3].Trim();
+                        degreeSummary[degree] = degreeSummary.GetValueOrDefault(degree, 0) + 1;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Return empty dictionary if file can't be read
+                return new Dictionary<string, int>();
+            }
+
+            return degreeSummary;
+        }
+
+        // Problem 3: IsAnagram - Using Dictionary for character counting
+        public static bool IsAnagram(string word1, string word2)
+        {
+            // Remove spaces and convert to lowercase
+            word1 = word1.Replace(" ", "").ToLower();
+            word2 = word2.Replace(" ", "").ToLower();
+
+            // If lengths are different, they can't be anagrams
+            if (word1.Length != word2.Length) return false;
+
+            var charCount = new Dictionary<char, int>();
+
+            // Count characters in first word
+            foreach (char c in word1)
+            {
+                charCount[c] = charCount.GetValueOrDefault(c, 0) + 1;
+            }
+
+            // Decrement counts for second word
+            foreach (char c in word2)
+            {
+                if (!charCount.ContainsKey(c)) return false;
+                charCount[c]--;
+                if (charCount[c] == 0) charCount.Remove(c);
+            }
+
+            // If dictionary is empty, all characters matched
+            return charCount.Count == 0;
+        }
+
+        // Problem 5: Earthquake JSON Data
+        public static string[] EarthquakeDailySummary()
+        {
+            try
+            {
+                string jsonData = File.ReadAllText("../../../earthquakes.json");
+                var featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(jsonData);
+                return featureCollection?.Features?
+                    .Select(f => $"{f.Properties.Place} - Mag {f.Properties.Mag:F2}")
+                    .ToArray() ?? Array.Empty<string>();
+            }
+            catch (Exception)
+            {
+                return Array.Empty<string>();
+            }
+        }
     }
 
-    /// <summary>
-    /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
-    /// new word.  A dictionary is used to solve the problem.
-    /// 
-    /// Examples:
-    /// is_anagram("CAT","ACT") would return true
-    /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
-    /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
-    /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
-    /// using the [] notation.
-    /// </summary>
-    public static bool IsAnagram(string word1, string word2)
+    // Problem 4: Maze implementation
+    public class Maze
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        private readonly Dictionary<(int, int), (bool left, bool right, bool up, bool down)> _maze;
+        private (int x, int y) _currentPosition;
+
+        public Maze(Dictionary<(int, int), bool[]> maze)
+        {
+            // Convert the bool[] to named tuple for better readability
+            _maze = maze.ToDictionary(
+                kvp => kvp.Key,
+                kvp => (
+                    left: kvp.Value[0],
+                    right: kvp.Value[1],
+                    up: kvp.Value[2],
+                    down: kvp.Value[3]
+                )
+            );
+            _currentPosition = (1, 1); // Starting position
+        }
+
+        public string GetStatus()
+        {
+            return $"Current location (x={_currentPosition.x}, y={_currentPosition.y})";
+        }
+
+        public (int x, int y) MoveLeft()
+        {
+            if (!_maze[_currentPosition].left)
+                throw new InvalidOperationException("Can't go that way!");
+            _currentPosition.x--;
+            return _currentPosition;
+        }
+
+        public (int x, int y) MoveRight()
+        {
+            if (!_maze[_currentPosition].right)
+                throw new InvalidOperationException("Can't go that way!");
+            _currentPosition.x++;
+            return _currentPosition;
+        }
+
+        public (int x, int y) MoveUp()
+        {
+            if (!_maze[_currentPosition].up)
+                throw new InvalidOperationException("Can't go that way!");
+            _currentPosition.y++;
+            return _currentPosition;
+        }
+
+        public (int x, int y) MoveDown()
+        {
+            if (!_maze[_currentPosition].down)
+                throw new InvalidOperationException("Can't go that way!");
+            _currentPosition.y--;
+            return _currentPosition;
+        }
     }
 
-    /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
-    /// United States Geological Service (USGS) consisting of earthquake data.
-    /// The data will include all earthquakes in the current day.
-    /// 
-    /// JSON data is organized into a dictionary. After reading the data using
-    /// the built-in HTTP client library, this function will return a list of all
-    /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
-    /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
-    /// </summary>
-    public static string[] EarthquakeDailySummary()
+    // Classes for JSON deserialization
+    public class FeatureCollection
     {
-        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-        using var client = new HttpClient();
-        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-        using var reader = new StreamReader(jsonStream);
-        var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        [JsonProperty("features")]
+        public List<Feature> Features { get; set; } = new List<Feature>();
+    }
 
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+    public class Feature
+    {
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; } = new Properties();
+    }
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+    public class Properties
+    {
+        [JsonProperty("place")]
+        public string Place { get; set; } = string.Empty;
+
+        [JsonProperty("mag")]
+        public double Mag { get; set; }
     }
 }
